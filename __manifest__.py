8613dd8bf9a1352b153926e804b789fde856c868
Fernando Fuentes <fernandofuentesfullstack@gmail.com>
2020-06-12 15:24:35 +0200

    Creación de README en local

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "gym",
+    'summary': """Gym management""",
+    'description': """Gym management:classes, users, material...""",
+    'author': "Fernando Fuentes",
+    'category': 'Gym',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': ['views/gymclass_view.xml'],
+    'demo': [ ],
+    'application': True,
+}
---
+++ models/gymclass.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class gymclass(models.Model):
+    _name = 'gym.gymclass'
+    
+    name = fields.Char('Nombre', size=64, required=True)
+    start = fields.Datetime('Comienzo',required=True, autodate = True)
+    end = fields.Datetime('Fin',required=True, autodate = True)
+    capacity =  fields.Integer("Capacidad") 
+    activityType = fields.Selection([('dance','Baile'),
+                                     ('aerobic','Aerobica'),
+                                     ('anaerobic','Anaerobica'),
+                                     ('relax','Relax'),],
+                                     'Tipo de actividad')
